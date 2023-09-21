08687957429e722cad07e573ccf7506b8f6b0e1d
Karthik K <125796499+kskarthik12@users.noreply.github.com>
2023-09-21 09:14:36 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,16 @@
+class Person {
+    constructor(firstName, lastName, age, gender, address) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.gender = gender;
+      this.address = address;
+    }
+}
+        const mine = new Person("John","David",50,"Male","R. VASUDEVAN No. 1, New Bangaru Naidu Colony, K.K. Nagar, Chennai - 600078")
+        const mine1 = new Person("John1","David1",21,"Male","B.20 II Floor, I.H.F.D. Nagar, Pallavaram, Chennai - 600 043")
+        
+
+        console.log(mine);
+        console.log(mine1);
+    
